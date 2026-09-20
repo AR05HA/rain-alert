ba07c9b15663665260ca304f03b3d7c83d778271
Arosha Chamika <aroshachamik@gmail.com>
2026-09-20 12:34:37 +0530

    Refactor email sending mechanism
    Refactor email sending to use EmailMessage class for better structure.

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import os
 import smtplib
 import requests
+from email.message import EmailMessage
 
 API_KEY = os.environ.get("OWM_API_KEY")
 my_email = os.environ.get("MY_EMAIL")
 password = os.environ.get("MY_PASSWORD")
 to_email = os.environ.get("TO_EMAIL")
 
-my_lat = os.environ.get("MY_LAT") 
-my_lng = os.environ.get("MY_LNG") 
+my_lat = os.environ.get("MY_LAT")
+my_lng = os.environ.get("MY_LNG")
 
 parameters = {
     "lat": my_lat,
@@ -26,16 +27,18 @@ data = response.json()
 will_rain = False
 
 for hour_data in data["list"]:
-  for condition in hour_data["weather"]:
-    if condition["id"] < 700:
-      will_rain = True
+    for condition in hour_data["weather"]:
+        if condition["id"] < 700:
+            will_rain = True
 
 if will_rain:
-  with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs=to_email,
-        msg="Subject:🌧️ Rain Alert: Grab an Umbrella!\n\nRain is expected in your area. Don't forget your umbrella before heading out!".encode('utf-8')
-    )
+    msg = EmailMessage()
+    msg["Subject"] = "🌧️ Rain Alert: Grab an Umbrella!"
+    msg["From"] = my_email
+    msg["To"] = to_email
+    msg.set_content("Rain is expected in your area. Don't forget your umbrella before heading out!")
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.send_message(msg)
